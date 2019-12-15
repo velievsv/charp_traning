161224df64ab404059d246ec8b34c820d8a77495
velievsv <velievsv@gmail.com>
2019-12-15 22:09:47 +0300

    Создано консольное приложение.

---
+++ addressbook-web-tests1/Generate-data-webaddresbook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WebAddressbookTest;
+using System.IO;
+
+namespace Generate_data_webaddresbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < count; i++)
+            {
+                writer.WriteLine(string.Format("${0},${1},${2}",
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10)));
+            }
+            writer.Close();
+        }
+    }
+}
--- addressbook-web-tests1/addressbook-web-tests1/Tests/GroupCreationsTest.cs
+++ addressbook-web-tests1/addressbook-web-tests1/Tests/GroupCreationsTest.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WebAddressbookTest
 {
@@ -25,9 +26,26 @@ namespace WebAddressbookTest
             return groups;
         }
 
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+           string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(",");
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            
+            }
+            return groups;
+        }
+        
   
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTests(GroupData group)
         {
            
